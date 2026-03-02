d79adc4efbf2bfa48841d26e0643975c6030920f
Nico <nicolaasb@pm.me>
2026-03-02 22:49:07 +0100

    Update vite config with allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vinext()],
+  server: {
+    allowedHosts: [".tendon.dev", ".vercel.app", ".vercel.run"],
+  },
 });
